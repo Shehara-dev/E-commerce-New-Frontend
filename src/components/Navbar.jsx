2f920632e53cdf8aa13973070eebd67473b571c7
Shehara <sheharasiriwardana21@gmail.com>
2026-07-16 13:17:20 +0530

    update navbar title to reflect new branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         
         <Link to="/" className="text-2xl font-extrabold text-blue-600 hover:text-blue-700 transition">
-          E-Online Shop
+          E-Computer Accessories Shop
         </Link>
 
         
